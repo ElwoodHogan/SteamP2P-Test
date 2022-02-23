a453dbcba25ef6ded7b8b73e36dbd94745f609fb
Leon Hogan <73083286+ElwoodHogan@users.noreply.github.com>
2022-02-23 16:11:55 -0600

    test 6

--- Assets/MoveSphere.cs
+++ Assets/MoveSphere.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static FrontMan;
-
+using Unity.Netcode;
 public class MoveSphere : MonoBehaviour
 {
     public float AccelForce;
@@ -11,6 +11,12 @@ public class MoveSphere : MonoBehaviour
         print("doiwn");
     }
     private void OnMouseDrag()
+    {
+        MovingSphereServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership =false)]
+    public void MovingSphereServerRpc()
     {
         Vector3 MouseYPos = FM.MainCam.ScreenToWorldPoint(Input.mousePosition);
         MouseYPos = Vector3.Normalize(new Vector3(MouseYPos.x, MouseYPos.y, 0));
